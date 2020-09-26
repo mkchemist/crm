370a4874b9c8abbca442ffbda29e64ad52103c27
mkchemist <m.k_chemist@yahoo.com>
2020-09-26 23:01:11 +0200

    creating rep pharmacy report

--- app/PharmacyReport.php
+++ app/PharmacyReport.php
@@ -9,7 +9,13 @@ class PharmacyReport extends Model
   protected $fillable = [
     'pharmacy_id',
     'user_id',
+    'visit_date',
     'general_feedback',
     'products'
   ];
+
+  public function pharmacy()
+  {
+    return $this->belongsTo('App\Pharmacy', 'pharmacy_id', 'id');
+  }
 }
