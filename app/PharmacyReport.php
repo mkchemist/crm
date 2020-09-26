dccd67138f7647e406ce8e2fd46bc56df920aa72
Mohamed Kamal <m.k_chemist@yahoo.com>
2020-09-26 19:05:43 +0200

    updating pharmacy report model

--- app/PharmacyReport.php
+++ app/PharmacyReport.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class PharmacyReport extends Model
 {
-    //
+  protected $fillable = [
+    'pharmacy_id',
+    'user_id',
+    'general_feedback',
+    'products'
+  ];
 }
