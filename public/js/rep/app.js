18698d2406c62cba9b76b759a5d4b46a944d7ea2
mkchemist <m.k_chemist@yahoo.com>
2020-10-22 15:33:42 +0200

    adding telescope

--- public/js/rep/app.js
+++ public/js/rep/app.js
@@ -51396,75 +51396,78 @@ __webpack_require__.r(__webpack_exports__);
  *
  */
 var CUSTOMERS_TABLE_HEADS = [{
-  title: 'ID',
-  name: 'id'
+  title: "ID",
+  name: "id"
 }, {
-  title: 'Name',
-  name: 'name'
+  title: "Name",
+  name: "name"
 }, {
-  title: 'Specialty',
-  name: 'specialty'
+  title: "Specialty",
+  name: "specialty"
 }, {
-  title: 'Title',
-  name: 'title'
+  title: "Title",
+  name: "title"
 }, {
-  title: 'Workplace',
-  name: 'workplace'
+  title: "Workplace",
+  name: "workplace"
 }, {
-  title: 'Param',
-  name: 'parameter'
+  title: "Param",
+  name: "parameter"
 }, {
-  title: 'Current Freq.',
-  name: 'current_freq'
+  title: "Current Freq.",
+  name: "current_freq"
 }, {
-  title: 'Next Freq.',
-  name: 'next_freq'
+  title: "Next Freq.",
+  name: "next_freq"
+}, {
+  title: "Plans",
+  name: "plans"
+}, {
+  title: "Visits",
+  name: "report"
+}, {
+  title: "Address",
+  name: "address"
+}, {
+  title: "Brick",
+  name: "brick"
+}, {
+  title: "Area",
+  name: "area"
+}];
+var DM_CUSTOMERS_HEADS = [{
+  title: "ID",
+  name: "id"
+}, {
+  title: "Area",
+  name: "area"
+}, {
+  title: "Brick",
+  name: "brick"
+}, {
+  title: "Name",
+  name: "name"
+}, {
+  title: "Specialty",
+  name: "specialty"
+}, {
+  title: "Parameters",
+  name: "parameter"
+}, {
+  title: "Current Freq.",
+  name: "current_freq"
+}, {
+  title: "Next Freq.",
+  name: "next_freq"
 }, {
   title: 'Plans',
   name: 'plans'
 }, {
-  title: 'Visits',
-  name: 'report'
+  title: "Reports",
+  name: "reports"
 }, {
-  title: 'Address',
-  name: 'address'
-}, {
-  title: 'Brick',
-  name: 'brick'
-}, {
-  title: 'Area',
-  name: 'area'
-}];
-var DM_CUSTOMERS_HEADS = [{
-  title: 'ID',
-  name: 'id'
-}, {
-  title: 'Area',
-  name: 'area'
-}, {
-  title: 'Brick',
-  name: 'brick'
-}, {
-  title: 'Name',
-  name: 'name'
-}, {
-  title: 'Specialty',
-  name: 'specialty'
-}, {
-  title: 'Parameters',
-  name: 'parameter'
-}, {
-  title: 'Current Freq.',
-  name: 'current_freq'
-}, {
-  title: 'Next Freq.',
-  name: 'next_freq'
-}, {
-  title: 'Reports',
-  name: 'reports'
-}, {
-  title: 'Address',
-  name: 'address'
+  title: "Address",
+  name: "address"
 }];
 
 /***/ }),
--- resources/js/helpers/constants.js
+++ resources/js/helpers/constants.js
@@ -5,98 +5,102 @@
  */
 export const CUSTOMERS_TABLE_HEADS = [
   {
-    title: 'ID',
-    name: 'id'
+    title: "ID",
+    name: "id"
   },
   {
-    title: 'Name',
-    name: 'name'
+    title: "Name",
+    name: "name"
   },
   {
-    title: 'Specialty',
-    name: 'specialty'
+    title: "Specialty",
+    name: "specialty"
   },
   {
-    title: 'Title',
-    name: 'title'
+    title: "Title",
+    name: "title"
   },
   {
-    title: 'Workplace',
-    name: 'workplace'
+    title: "Workplace",
+    name: "workplace"
   },
   {
-    title: 'Param',
-    name: 'parameter'
+    title: "Param",
+    name: "parameter"
   },
   {
-    title: 'Current Freq.',
-    name: 'current_freq'
+    title: "Current Freq.",
+    name: "current_freq"
   },
   {
-    title: 'Next Freq.',
-    name: 'next_freq'
+    title: "Next Freq.",
+    name: "next_freq"
+  },
+  {
+    title: "Plans",
+    name: "plans"
+  },
+  {
+    title: "Visits",
+    name: "report"
+  },
+  {
+    title: "Address",
+    name: "address"
+  },
+  {
+    title: "Brick",
+    name: "brick"
+  },
+  {
+    title: "Area",
+    name: "area"
+  }
+];
+
+export const DM_CUSTOMERS_HEADS = [
+  {
+    title: "ID",
+    name: "id"
+  },
+  {
+    title: "Area",
+    name: "area"
+  },
+  {
+    title: "Brick",
+    name: "brick"
+  },
+  {
+    title: "Name",
+    name: "name"
+  },
+  {
+    title: "Specialty",
+    name: "specialty"
+  },
+  {
+    title: "Parameters",
+    name: "parameter"
+  },
+  {
+    title: "Current Freq.",
+    name: "current_freq"
+  },
+  {
+    title: "Next Freq.",
+    name: "next_freq"
   },
   {
     title: 'Plans',
     name: 'plans'
   },
   {
-    title: 'Visits',
-    name: 'report'
+    title: "Reports",
+    name: "reports"
   },
   {
-    title: 'Address',
-    name: 'address'
-  },
-  {
-    title: 'Brick',
-    name: 'brick'
-  },
-  {
-    title: 'Area',
-    name: 'area'
+    title: "Address",
+    name: "address"
   }
 ];
-
-export const DM_CUSTOMERS_HEADS = [
-  {
-    title: 'ID',
-    name:'id'
-  },
-  {
-    title:'Area',
-    name:'area'
-  },
-  {
-    title: 'Brick',
-    name: 'brick'
-  },
-  {
-    title: 'Name',
-    name:'name'
-  },
-  {
-    title: 'Specialty',
-    name:'specialty'
-  },
-  {
-    title: 'Parameters',
-    name: 'parameter'
-  },
-  {
-    title: 'Current Freq.',
-    name:'current_freq'
-  },
-  {
-    title:'Next Freq.',
-    name:'next_freq'
-  },
-    {
-      title: 'Reports',
-      name: 'reports'
-    },
-  {
-    title: 'Address',
-    name:'address'
-  },
-]
