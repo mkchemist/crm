3e0224a620e38d4fad009ef4849978fc76d7366e
mkchemist <m.k_chemist@yahoo.com>
2020-10-24 02:05:22 +0200

    create customer card route

--- resources/js/dm/router/customers-routes.js
+++ resources/js/dm/router/customers-routes.js
@@ -2,6 +2,8 @@ import CustomersPage from "../views/Customers.vue"
 import ActiveCustomers from "../views/customers/ActiveCustomers.vue"
 import InActiveCustomers from "../views/customers/InActiveCustomers.vue"
 import CustomerFavoriteList from "../views/customers/CustomersFavoriteList.vue";
+import CustomerCard from "../views/customers/CustomerCard.vue";
+
 export default {
   path: '/customers',
   component: CustomersPage,
@@ -17,6 +19,10 @@ export default {
     {
       path: 'favorite-list',
       component: CustomerFavoriteList
+    },
+    {
+      path: 'view/:id',
+      component: CustomerCard
     }
   ]
 }
