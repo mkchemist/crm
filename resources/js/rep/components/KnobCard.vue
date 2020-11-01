cb5c3f8d880790be87a3a4a85243ad26f8b87fdd
mkchemist <m.k_chemist@yahoo.com>
2020-11-02 00:29:23 +0200

    create quick analysis section in rep home page

---
+++ resources/js/rep/components/KnobCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div>
+      <div class="text-center">
+        <knob-control
+          :max="max"
+          v-model="val"
+          :stroke-width="8"
+          :animation="{
+            animated: true,
+            animateValue: true,
+            animationDuration: '5000',
+            animationFunction: 'linear'
+          }"
+          :step-size="1"
+          :readonly="true"
+          primary-color="#38c172"
+          text-color="#38c172"
+        />
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import KnobControl from "vue-knob-control";
+export default {
+  components: {
+    KnobControl
+  },
+  props: ['val', 'max']
+};
+</script>
+
+<style></style>
--- resources/js/rep/views/Home.vue
+++ resources/js/rep/views/Home.vue
@@ -6,6 +6,27 @@
             <today-plan />
           </div>
         </div>
+        <div class="col-lg-9">
+          <div class="row mx-auto">
+            <div class="col-lg">
+
+            </div>
+            <div class="col-lg-auto">
+              <p class="lead text-center text-muted">Quick Analysis</p>
+              <hr>
+              <div class="border rounded p-2">
+                <knob-card :val="reportPercent" :max="100">
+                  <p class="text-muted font-italic small">Plan achievment %</p>
+                </knob-card>
+              </div>
+              <div class="border rounded p-2 my-1">
+                <knob-card :val="planPercent" :max="planPercent > 100 ? 200 : 100">
+                  <p class="text-muted font-italic small">Plan versus Frequency %</p>
+                </knob-card>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
   </div>
 </template>
@@ -17,10 +38,33 @@
  *
  */
 import TodayPlan from "../components/TodayPlan";
+import KnobCard from "../components/KnobCard"
 export default {
   components: {
-    TodayPlan
+    TodayPlan,
+    KnobCard
   },
+  created() {
+    this.$store.dispatch("getPlanner").then(() => {
+      this.$store.dispatch("reportGetAll");
+    }).then(() => {
+      this.$store.dispatch("customerGetAll")
+    });
+  },
+  computed: {
+    reportPercent() {
+      let reports = this.$store.getters.pmVisits;
+      let plans = this.$store.getters.plans;
+      let percent = (reports.length / plans.length) * 100;
+      return parseInt(percent.toFixed(1));
+    },
+    planPercent() {
+      let freq = 0;
+      this.$store.getters.all.map(customer => freq += customer.current_freq)
+      let plans = this.$store.getters.plans;
+      return parseInt(((plans.length/freq)*100).toFixed(1));
+    }
+  }
 }
 </script>
 
