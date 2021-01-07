b3b28f452e353184fb370a4a5f063756e5a90fac
mkchemist <m.k_chemist@yahoo.com>
2021-01-07 21:43:57 +0200

    fixing admin missed visit query

--- app/Http/Controllers/Api/V1/Admin/Reports/MissedVisitsReportController.php
+++ app/Http/Controllers/Api/V1/Admin/Reports/MissedVisitsReportController.php
@@ -16,7 +16,7 @@ class MissedVisitsReportController extends Controller
       $activeCycle = new ActiveCycleSetting;
       $active =$activeCycle->all();
       $today = date('20y-m-d');
-      $reports = DB::table('planners as plan')
+      /* $reports = DB::table('planners as plan')
       ->select(
         'plan.plan_date as Date',
         'customer.name as Customer',
@@ -42,7 +42,36 @@ class MissedVisitsReportController extends Controller
         $join->on('report.customer_id', '=', 'plan.customer_id');
         $join->on('report.user_id', '=', 'plan.user_id');
         $join->on('report.visit_date', '=', 'plan.plan_date');
-      })->where('report.visit_date',NULL);
+      })->where('report.visit_date',NULL); */
+      $reports = DB::table('planners AS plan')
+      ->select(
+        'plan.plan_date AS Date',
+        'customer.name as Customer',
+        'customer.specialty as Specialty',
+        'customer.area as Area',
+        'customer.brick as Brick',
+        'customer.district as District',
+        'customer.territory as Territory',
+        'rep.name as Rep',
+        'plan.user_id as user_id',
+        'param.current as Parameter',
+        'freq.current as Freq',
+        DB::raw('count(plan.plan_date) AS countOfPlans'),
+        DB::raw('count(visit.visit_date) AS countOfVisits'),
+        DB::raw('count(plan.plan_date) - count(visit.visit_date) as diff')
+      )->join('customers as customer', 'customer.id', '=', 'plan.customer_id')
+      ->join('users as rep', 'rep.id', '=', 'plan.user_id')
+      ->leftJoin('customer_parameters as param', function($join) {
+        $join->on('param.customer_id', '=', 'plan.customer_id');
+        $join->on('param.user_id', '=', 'plan.user_id');
+      })->leftJoin('customer_frequencies as freq', function($join) {
+        $join->on('freq.customer_id', '=', 'plan.customer_id');
+        $join->on('freq.user_id', '=', 'plan.user_id');
+      })->leftJoin('customer_reports AS visit', function($join) {
+        $join->on('visit.customer_id', '=', 'plan.customer_id');
+        $join->on('visit.user_id', '=', 'plan.user_id');
+      })->groupBy('Date','Rep','user_id','Customer','Specialty','Parameter','Freq','Area','Brick','District','Territory')
+      ->having('diff','>',0);
       $userData = null;
       if($user !== null) {
         $userData = User::find($user);
