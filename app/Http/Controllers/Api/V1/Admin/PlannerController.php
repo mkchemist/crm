2af82edab02ace94024d6d54c6391134416e250b
mkchemist <m.k_chemist@yahoo.com>
2021-02-21 21:52:05 +0200

    fixing planner issue

--- app/Http/Controllers/Api/V1/Admin/PlannerController.php
+++ app/Http/Controllers/Api/V1/Admin/PlannerController.php
@@ -24,7 +24,7 @@ class PlannerController extends Controller
     /**
      * pm plans
      */
-    $pm = DB::table('planners as plan')
+   /*  $pm = DB::table('planners as plan')
     ->select(
       'plan.plan_date as Date',
       'user.name as Rep',
@@ -54,6 +54,9 @@ class PlannerController extends Controller
     if($data) {
       $pm = CycleHelper::getCycleData($pm,'plan.plan_date');
     }
+    $pm = $pm->get(); */
+    $pm = Planner::query();
+    $pm = CycleHelper::getCycleData($pm,'plan_date');
     $pm = $pm->get();
   /*   $pm = Planner::with([
       'customer','customer.params','user','customer.frequency'
